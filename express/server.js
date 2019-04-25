00ce3251ec57053a895cc60a7bc9744efccd54c3
Julystep <41725214+Julystep@users.noreply.github.com>
2019-04-25 20:57:08 +0800

    express版的留言本
    用express封装的留言本

---
+++ express/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const fs = require('fs');
+const template = require('art-template');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let comments = [];
+
+app.use('/public/', express.static('./public/'));                   //请求内部的资源，随便用
+
+app.engine('html', require('express-art-template'));               //配置模板引擎
+
+/* 解析post中的数据专用 */
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('index.html', {
+        comments : comments
+    });
+});
+
+app.get('/post', (req, res) => {
+    res.render('post.html');
+});
+
+app.post('/post', (req, res) => {
+    
+    let comment = req.body;                                       //只能拿get请求参数
+    console.log(comment);
+    comment.time = new Date();
+    comments.unshift(comment);
+    res.redirect('/');
+
+});
+
+
+
+app.listen(3000, () => {
+    console.log('running...');
+})
